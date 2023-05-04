194adc92b5f9a271dda13f109ee03bcf12d34286
Juliane Cassidy <julianecassidy@gmail.com>
2023-05-04 13:31:01 -0600

    add photo routes

--- app.py
+++ app.py
@@ -3,12 +3,15 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request, g
+from flask_cors import CORS
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy import or_
 from sqlalchemy.exc import IntegrityError
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import BadRequest, Unauthorized
 import jwt
+import uuid
+import logging
 
 
 from models import db, connect_db, User, Photo, Match, Like, Dislike
@@ -17,6 +20,7 @@ from auth_middleware import require_user
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
@@ -24,6 +28,8 @@ app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 UPLOAD_FOLDER = "uploads"
 
 connect_db(app)
+
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
@@ -36,29 +42,29 @@ debug = DebugToolbarExtension(app)
 ################################################################################
 #  User Signup/Login/Logout
 
-@app.before_request
-def add_user_to_g():
-    """If there is a user logged in, add the current user to Flask global."""
+# @app.before_request
+# def add_user_to_g():
+#     """If there is a user logged in, add the current user to Flask global."""
 
-    token = None
-    if "Authorization" in request.headers:
-        token = request.headers["Authorization"]
+#     token = None
+#     if "Authorization" in request.headers:
+#         token = request.headers["Authorization"]
 
-    if token:
-        try:
-            data = jwt.decode(token, os.environ['SECRET_KEY'], algorithms=["HS256"])
-            print("IS THERE ERROR HERE?")
-            current_user = User.query.get(data["username"])
-            if current_user is None:
-                raise Unauthorized("Invalid token.")
-            else:
-                g.user = current_user
+#     if token:
+#         try:
+#             data = jwt.decode(token, os.environ['SECRET_KEY'], algorithms=["HS256"])
+#             print("IS THERE ERROR HERE?")
+#             current_user = User.query.get(data["username"])
+#             if current_user is None:
+#                 raise Unauthorized("Invalid token.")
+#             else:
+#                 g.user = current_user
             
-        except Exception as e:
-            raise Unauthorized("Invalid token")
+#         except Exception as e:
+#             raise Unauthorized("Invalid token")
     
-    else:
-        g.user = None
+#     else:
+#         g.user = None
     
     
 
@@ -132,13 +138,18 @@ def update_user_profile(username):
     
     user = User.query.get_or_404(username)
 
-    user.name = request.json["name"]
-    user.hobbies=request.json["hobbies"],
-    user.interests=request.json["interests"],
-    user.postal_code=request.json["postal_code"],
-    user.search_radius=request.json["search_radius"]
+    try:
+        user.name = request.json["name"]
+        user.hobbies=request.json["hobbies"],
+        user.interests=request.json["interests"],
+        user.postal_code=request.json["postal_code"],
+        user.search_radius=request.json["search_radius"]
 
-    db.session.commit()
+        db.session.commit()
+    
+    except:
+        db.session.rollback()
+        raise BadRequest
 
     updated_user_instance = User.query.get(username)
     updated_user = updated_user_instance.serialize()
@@ -174,23 +185,54 @@ def delete_user(username):
     return f"{username} deleted"
 
 
+################################################################################
+# Photo Routes
+
+@app.get("/<username>/photos")
+# @require_user
+def get_photos(username):
+    """Return all photos for a given username. Must be logged in to access."""
+    return "hello"
+    image_ids = Photo.query.filter_by(user_id=username).all()
+
+    image_urls = User.get_user_images(image_ids)
+    return jsonify(image_urls)
 
 
-# @app.route("/")
-# def home():
-#     """Render temporary homepage with image submission form and viewable images."""
-#     images = get_user_image()
-#     print("IMAGES", images)
-#     return render_template('base.html', images=images)
+@app.post("/<username>/photos")
+# @require_user
+def upload_photo(username):
+    """Upload image file from request data to AWS S3. Must be logged in as same
+    user at endpoint to access."""
 
+    print("API ROUTE UPLOAD_PHOTO")
 
-# @app.route("/upload", methods=["POST"])
-# def upload():
-#     """Upload image file from form to AWS S3."""
+    # if username != g.user.username:
+    #     raise Unauthorized
 
-#     if request.method == "POST":
-#         f = request.files['image-file']
-#         f.save(os.path.join(UPLOAD_FOLDER, secure_filename(f.filename)))
-#         print("FILENAME IN APP", f"uploads/{f.filename}")
-#         upload_photo(f"uploads/{f.filename}")
-#         return redirect("/")
+    file = request.files['image-file']
+    file.filename = uuid()
+
+    try:
+        ## save file to local folder
+        file.save(os.path.join(UPLOAD_FOLDER, secure_filename(file.filename)))
+
+        ## save file to S3
+        upload_photo(f"uploads/{file.filename}")
+
+        ## save file name to db
+        image_file = Photo(
+            file_name = file.filename,
+            user_id=username
+        )
+        db.session.add(image_file)
+        db.session.commit()
+
+        ## remove file from local folder
+        os.remove(f"uploads/{file.filename}")
+
+        return jsonify("image uploaded")
+    
+    except:
+        db.session.rollback()
+        raise BadRequest
--- models.py
+++ models.py
@@ -18,12 +18,12 @@ DEFAULT_IMAGE_FILE = "default-image.jpg"
 DEFAULT_IMAGE_OBJECT = "username-12345"
 SECRET_KEY = os.environ['SECRET_KEY']
 
-# s3_client = boto3.client(
-#     's3', 
-#     # aws_access_key_id = os.environ['ACCESS_KEY'], 
-#     # aws_secret_access_key = os.environ['AWS_SECRET_KEY'], 
-#     config=Config(signature_version='s3v4')
-#     ) 
+s3_client = boto3.client(
+    's3', 
+    aws_access_key_id = os.environ['ACCESS_KEY'], 
+    aws_secret_access_key = os.environ['AWS_SECRET_KEY'], 
+    config=Config(signature_version='s3v4')
+    ) 
 
 AWS_S3_REGION_NAME = "us-east-2"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
@@ -47,43 +47,43 @@ class Photo(db.Model):
 
     ## Relationship User <-> Photos
 
-    # @classmethod
-    # def upload_photo(self, file_name, bucket="friender_user_photos"):
-    #     """Uploads a file to Friender user photos bucket on S3. 
-    #     Requires file_name and object_name of the image.
-    #     Returns true if image successfully uploaded or false if not."""
+    @classmethod
+    def upload_photo(cls, file_name, bucket="friender_user_photos"):
+        """Uploads a file to Friender user photos bucket on S3. 
+        Requires file_name and object_name of the image.
+        Returns true if image successfully uploaded or false if not."""
 
-    #     print("FILENAME IN MODELS", file_name)
-    #     try:
-    #         response = s3_client.upload_file(file_name, bucket, file_name)
-    #     except ClientError as e:
-    #         print(e)
-    #         return False
-    #     return True
+        print("UPLOAD_PHOTO")
 
-    # @classmethod
-    # def get_user_image(bucket="frienderuserphotos"):
-    #     """Generate URL to provide image source for a user's image."""
+        try:
+            response = s3_client.upload_file(file_name, bucket, file_name)
+        except ClientError as e:
+            print(e)
+            return False
+        return True
 
-    #     print("GET_USER_IMAGE")
+    @classmethod
+    def get_user_image(cls, image_ids, bucket="frienderuserphotos"):
+        """Generate URL to provide image source for a user's image."""
 
-    #     public_urls = []
-    #     user_photos = ['uploads/rv.jpg',
-    #                    'uploads/tree.jpg', 'uploads/MetLife.png']
+        print("GET_USER_IMAGE")
 
-    #     try:
-    #         for item in user_photos:
-    #             presigned_url = s3_client.generate_presigned_url(
-    #                 'get_object',
-    #                 Params={
-    #                     'Bucket': bucket,
-    #                     'Key': item},
-    #                 ExpiresIn=3600)
-    #             public_urls.append(presigned_url)
-    #     except Exception as e:
-    #         pass
-    #     # print("[INFO] : The contents inside show_image = ", public_urls)
-    #     return public_urls
+        public_urls = []
+        image_paths = [f"uploads/{id}" for id in image_ids]
+
+        try:
+            for path in image_paths:
+                presigned_url = s3_client.generate_presigned_url(
+                    'get_object',
+                    Params={
+                        'Bucket': bucket,
+                        'Key': path},
+                    ExpiresIn=3600)
+                public_urls.append(presigned_url)
+        except Exception as e:
+            pass
+        # print("[INFO] : The contents inside show_image = ", public_urls)
+        return public_urls
     
 
 class Match(db.Model):
